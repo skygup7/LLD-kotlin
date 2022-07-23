8a5f0c0215f368faea2c455bed69d741ac0a4d94
skygup7 <sky.gup7@gmail.com>
2022-07-23 19:48:34 +0530

    init commit. let's make the module

---
+++ snake-and-ladder/src/main/kotlin/lld/snakeandladder/SnakeAndLadderMain.kt
@@ -0,0 +1,10 @@
+package lld.snakeandladder
+
+import java.util.logging.Logger
+
+
+private val LOGGER = Logger.getLogger("SnakeAndLadder")
+
+fun main() {
+    LOGGER.info("application started")
+}
