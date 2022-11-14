29bae90cc9d5b2b082a0c4aeea2bb54654792e4f
Akash Gupta <sky.gup7@gmail.com>
2022-11-15 02:48:00 +0530

    feat: add new module and skeleton for swiggy (#13)
    * adds new module for swiggy and models file
    
    * adds some more skeleton files

---
+++ swiggy/src/main/kotlin/lld/swiggy/SwiggyMain.kt
@@ -0,0 +1,10 @@
+package lld.swiggy
+
+import org.slf4j.LoggerFactory
+
+
+private  val logger = LoggerFactory.getLogger("Swiggy")
+
+fun main() {
+    logger.info("Welcome to Swiggy")
+}
---
+++ swiggy/src/main/kotlin/lld/swiggy/service/impl/SwiggyUserServiceImpl.kt
@@ -0,0 +1,13 @@
+package lld.swiggy.service.impl
+
+import lld.swiggy.service.SwiggyUserService
+import org.slf4j.LoggerFactory
+
+class SwiggyUserServiceImpl(
+
+): SwiggyUserService {
+
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(SwiggyUserServiceImpl::class.java)
+    }
+}
