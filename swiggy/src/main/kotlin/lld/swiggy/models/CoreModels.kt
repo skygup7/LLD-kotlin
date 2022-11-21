70b9f3c6c994b7ca8cb9438470d0e2a32b7f5162
skygup7 <sky.gup7@gmail.com>
2022-11-21 12:48:51 +0530

    add some utility functions to core models

--- swiggy/src/main/kotlin/lld/swiggy/models/CoreModels.kt
+++ swiggy/src/main/kotlin/lld/swiggy/models/CoreModels.kt
@@ -1,9 +1,16 @@
 package lld.swiggy.models
 
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 data class Location(
-    val x: Long,
-    val y: Long
-)
+    val x: Double,
+    val y: Double
+) {
+    fun calculateDistanceFrom(location: Location): Double {
+        return sqrt((location.x - x).pow(2) + (location.y - y).pow(2))
+    }
+}
 
 enum class EntityPrefix {
     user,
@@ -24,4 +31,11 @@ data class EntityPublicId(
 data class Rating(
     val avgRating: Double,
     val ratingCount: Long
-)
+) {
+    fun calculateNewRating(rating: Long): Rating {
+        return Rating(
+            avgRating = (avgRating * ratingCount + rating) / (ratingCount + 1),
+            ratingCount = ratingCount + 1
+        )
+    }
+}
